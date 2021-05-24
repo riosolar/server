96ffd7177a72fb663d8baeb1384b3c07132b0001
riosolar <83927142+riosolar@users.noreply.github.com>
2021-05-24 05:03:19 -0300

    Create serverworker1.sh

---
+++ serverworker1.sh
@@ -0,0 +1,9 @@
+export USE_HOSTNAME=worker1.concienciadesarrollista.com
+echo $USE_HOSTNAME > /etc/hostname
+hostname -F /etc/hostname
+apt-get update -y
+apt-get upgrade -y
+apt-get install docker.io docker-compose -y
+docker swarm join --token SWMTKN-1-2e12ybkzxnb8qtlvbptjlqza50qvttuzdk843utk8b4wv8ze1o-chi2sz8rikiesl0w1590l8qoh 172.31.91.73:2377
+#sudo curl -L https://downloads.portainer.io/portainer-agent-stack.yml -o portainer-agent-stack.yml
+#sudo docker stack deploy -c portainer-agent-stack.yml portainer
