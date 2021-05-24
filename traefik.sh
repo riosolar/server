a99e3625f54935e90167ceddb1ebf1c369869db4
riosolar <83927142+riosolar@users.noreply.github.com>
2021-05-24 05:21:32 -0300

    Create traefik.sh

---
+++ traefik.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker network create --driver=overlay traefik-public
+export NODE_ID=$(docker info -f '{{.Swarm.NodeID}}')
+docker node update --label-add traefik-public.traefik-public-certificates=true $NODE_ID
+export EMAIL=devops.web.dev@gmail.com
+export DOMAIN=proxy.concienciadesarrollista.com
+export USERNAME=admin
+export PASSWORD=changethis
+export HASHED_PASSWORD=$(openssl passwd -apr1 $PASSWORD)
+curl -L dockerswarm.rocks/traefik.yml -o traefik.yml
+docker stack deploy -c traefik.yml traefik
+docker stack ps traefik
+
+
