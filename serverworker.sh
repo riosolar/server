e388584a5a9b7372e4babcb47a690e013435f5f3
riosolar <83927142+riosolar@users.noreply.github.com>
2021-05-24 03:17:22 -0300

    Update serverworker.sh

--- serverworker.sh
+++ serverworker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install docker.io docker-compose
+sudo apt-get install docker.io docker-compose -y
 sudo docker swarm join --token SWMTKN-1-6c47f8zm77b878dqjvc05c5ihrvkmujedeivwqx2b2djkmvhlu-3tfj3hmzauj05xs009gc61hla 18.190.161.252:2377	
 #sudo curl -L https://downloads.portainer.io/portainer-agent-stack.yml -o portainer-agent-stack.yml
 #sudo docker stack deploy -c portainer-agent-stack.yml portainer
