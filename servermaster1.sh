a83ea0b7f5a37892404b6c62dae961a2d1b966b3
riosolar <83927142+riosolar@users.noreply.github.com>
2021-05-24 04:57:38 -0300

    Update and rename server.sh to servermaster1.sh

---
+++ servermaster1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export USE_HOSTNAME=master1.concienciadesarrollista.com
+echo $USE_HOSTNAME > /etc/hostname
+hostname -F /etc/hostname
+apt-get update -y
+apt-get upgrade -y
+apt-get install docker.io docker-compose
+docker swarm init
+#sudo curl -L https://downloads.portainer.io/portainer-agent-stack.yml -o portainer-agent-stack.yml
+#sudo docker stack deploy -c portainer-agent-stack.yml portainer
