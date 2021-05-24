e12bce1bfa12aba726a071b21f57f955a96bf2a8
riosolar <83927142+riosolar@users.noreply.github.com>
2021-05-24 03:03:11 -0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get update -y
 sudo apt-get install docker.io docker-compose
-sudo docker swarm init
-sudo curl -L https://downloads.portainer.io/portainer-agent-stack.yml -o portainer-agent-stack.yml
-sudo docker stack deploy -c portainer-agent-stack.yml portainer
+sudo docker swarm init --advertise-addr 18.190.161.252	
+#sudo curl -L https://downloads.portainer.io/portainer-agent-stack.yml -o portainer-agent-stack.yml
+#sudo docker stack deploy -c portainer-agent-stack.yml portainer
