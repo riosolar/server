84480bf488023488faa131424de9450f630f8a5d
riosolar <83927142+riosolar@users.noreply.github.com>
2021-05-24 14:48:45 -0300

    Update master.sh

--- master.sh
+++ master.sh
@@ -1,10 +1,8 @@
-export USE_HOSTNAME=master2.concienciadesarrollista.com
+export USE_HOSTNAME=master3.concienciadesarrollista.com
 echo $USE_HOSTNAME > /etc/hostname
 hostname -F /etc/hostname
 apt-get update -y
 apt-get upgrade -y
 apt-get install docker.io docker-compose -y
 docker swarm join --token SWMTKN-1-3i8f6own3s2e4d4yd48ksg9edvowiivfgmt5e1sp0qioj0u5zs-3zr1shqnshuv06mkbkf1lwwu1 172.31.37.46:2377
-#sudo curl -L https://downloads.portainer.io/portainer-agent-stack.yml -o portainer-agent-stack.yml
-#sudo docker stack deploy -c portainer-agent-stack.yml portainer
-© 2021 GitHub, Inc.
+
