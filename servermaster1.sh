075da68baedbf93ee5f6f7e0adddb4535652072c
riosolar <83927142+riosolar@users.noreply.github.com>
2021-05-24 05:02:29 -0300

    Update servermaster1.sh

--- servermaster1.sh
+++ servermaster1.sh
@@ -5,7 +5,7 @@ echo $USE_HOSTNAME > /etc/hostname
 hostname -F /etc/hostname
 apt-get update -y
 apt-get upgrade -y
-apt-get install docker.io docker-compose
+apt-get install docker.io docker-compose -y
 docker swarm init
 #sudo curl -L https://downloads.portainer.io/portainer-agent-stack.yml -o portainer-agent-stack.yml
 #sudo docker stack deploy -c portainer-agent-stack.yml portainer
