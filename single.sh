673748762c33085d1d45ee6237ce75d9b0ccc9f5
riosolar <83927142+riosolar@users.noreply.github.com>
2021-05-25 18:39:28 -0300

    Update single.sh

--- single.sh
+++ single.sh
@@ -6,11 +6,11 @@ apt-get install docker.io docker-compose -y
 docker swarm init
 sudo curl -L https://raw.githubusercontent.com/riosolar/server/main/stack.yml -o stack.yml
 sudo docker stack deploy -c stack.yml wordpress
-export DOMAIN=panel2.concienciadesarrollista.com
-export NODE_ID=$(docker info -f '{{.Swarm.NodeID}}')
-docker node update --label-add portainer.portainer-data=true $NODE_ID
-curl -L dockerswarm.rocks/portainer.yml -o portainer.yml
-docker stack deploy -c portainer.yml portainer
+
+curl -L https://downloads.portainer.io/portainer-agent-stack.yml -o portainer-agent-stack.yml
+docker stack deploy -c portainer-agent-stack.yml portainer
+
+
 sudo curl -L https://raw.githubusercontent.com/riosolar/server/main/traefik.yml -o traefik.yml
 sudo docker stack deploy -c traefik.yml traefik ;
 sudo curl -L https://raw.githubusercontent.com/riosolar/server/main/monitor.yml -o monitor.yml
