6b0a4cf4ae6643ef7c0afae105401a62f258d92d
riosolar <83927142+riosolar@users.noreply.github.com>
2021-05-25 18:11:51 -0300

    Update single.sh

--- single.sh
+++ single.sh
@@ -8,3 +8,5 @@ sudo curl -L https://downloads.portainer.io/portainer-agent-stack.yml -o portain
 sudo docker stack deploy -c portainer-agent-stack.yml portainer
 sudo curl -L https://raw.githubusercontent.com/riosolar/server/main/stack.yml -o stack.yml
 sudo docker stack deploy -c stack.yml wordpress
+sudo curl -L https://raw.githubusercontent.com/riosolar/server/main/monitor.yml -o monitor.yml
+sudo docker stack deploy -c stack.yml monitor
