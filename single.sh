16ec5eb7bfdb7ca050c02becbedc6a8d6cf197df
riosolar <83927142+riosolar@users.noreply.github.com>
2021-05-25 18:30:27 -0300

    Update single.sh

--- single.sh
+++ single.sh
@@ -4,11 +4,11 @@ apt-get update -y
 apt-get upgrade -y
 apt-get install docker.io docker-compose -y
 docker swarm init
-sudo curl -L https://downloads.portainer.io/portainer-agent-stack.yml -o portainer-agent-stack.yml 
-sudo docker stack deploy -c portainer-agent-stack.yml portainer
-sudo curl -L https://raw.githubusercontent.com/riosolar/server/main/traefik.yml -o traefik.yml
-sudo docker stack deploy -c traefik.yml traefik
 sudo curl -L https://raw.githubusercontent.com/riosolar/server/main/stack.yml -o stack.yml
 sudo docker stack deploy -c stack.yml wordpress
+sudo curl -L https://raw.githubusercontent.com/riosolar/server/main/portainer.yml -o portainer.yml
+sudo docker stack deploy -c portainer.yml portainer
+sudo curl -L https://raw.githubusercontent.com/riosolar/server/main/traefik.yml -o traefik.yml
+sudo docker stack deploy -c traefik.yml traefik
 sudo curl -L https://raw.githubusercontent.com/riosolar/server/main/monitor.yml -o monitor.yml
 sudo docker stack deploy -c monitor.yml monitor
