60410cf3080e0e996918b5b9f69445ec4a9dfd16
riosolar <83927142+riosolar@users.noreply.github.com>
2021-05-27 23:53:49 -0300

    Update single.sh

--- single.sh
+++ single.sh
@@ -4,6 +4,5 @@ apt-get update -y
 apt-get upgrade -y
 apt-get install docker.io docker-compose -y
 docker swarm init
-curl -L https://raw.githubusercontent.com/riosolar/server/main/stack.yml -o stack.yml
-docker stack deploy -c stack.yml wordpress
+
 
