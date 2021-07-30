813ee6c9fa036e67d12a58376863456a9cfa1d69
riosolar <83927142+riosolar@users.noreply.github.com>
2021-07-30 15:23:52 -0300

    Update single.sh

--- single.sh
+++ single.sh
@@ -3,5 +3,8 @@
 export DOMAIN=riocuartonoticias.online
 export NODE_ID=$(docker info -f '{{.Swarm.NodeID}}')
 docker node update --label-add portainer.portainer-data=true $NODE_ID
+wget https://raw.githubusercontent.com/riosolar/server/main/wp.yml
+mv wp.yml docker-compose.yml
+docker stack deploy -c docker-compose.yml wordpress
 
 
